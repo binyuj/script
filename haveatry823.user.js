cc454e1bc63026c3289a730a1ca2ece837f0f32b
haveatry823 <401068081@qq.com>
2013-03-15 18:00:14 +0800

    fixed

--- haveatry823.user.js
+++ haveatry823.user.js
@@ -7,34 +7,34 @@
 // @downloadURL    https://github.com/haveatry823/script
 // @updateURL      https://github.com/haveatry823/script
 // @include        https://github.com/*/commit/*
+// @include        https://github.com/*/commits/*
 // @include        https://github.com/*/blame/*
 // @include        http://tieba.baidu.com/p/*
 // @include        http://tieba.baidu.com/f*
 // ==/UserScript==
 
-setTimeout(function(){
-	function route(keyword, fn) {
-		if (window.location.href.indexOf(keyword) !== -1) fn();
-	}
 
-	function $(exp){
-		if (/^#.+$/.test(exp)) return document.getElementById(exp.replace("#",""));
-		if (/^\..+$/.test(exp)) return document.getElementsByClassName(exp.replace(".",""));
-		return null
-	}
+function route(keyword, fn) {
+	var win = typeof unsafeWindow === "undefined" ? window : unsafeWindow
+	if (win.location.href.indexOf(keyword) === -1) return;
+	var cb = document.createElement("script");
+	cb.type = "text/javascript";
+	cb.textContent = "(" + fn.toString() + ")();";
+	document.head.appendChild(cb);
+}
 
-	route("github", function(){		
-		var div = $(".container")
-		if (div && div.length >0){
-			for(var i=0; i< div.length; i++){
-				div[i].style.width = screen.width * 0.9 + "px";
-			}
+route("github", function(win){		
+	var div = document.getElementsByClassName("container")
+	if (div && div.length >0){
+		for(var i=0; i< div.length; i++){
+			div[i].style.width = screen.width * 0.9 + "px";
 		}
-	});	
-		
-	route("tieba.baidu.com", function(){
-		var div = $(".l_banner")
-		if (div && div.length == 1) div[0].style.display = "none";
-		if (window["PageData"] && window["PageData"].is_sign_in == 0 && window["Sign_rank"] && window["Sign_rank"].sign_add) window["Sign_rank"].sign_add();		
-	});
-},300);
+	}
+});	
+	
+route("tieba.baidu.com", function(){
+	var div = document.getElementsByClassName("l_banner")
+	if (div && div.length == 1) div[0].style.display = "none";
+	if (window["PageData"] && window["PageData"].is_sign_in == 0 && window["Sign_rank"] && window["Sign_rank"].sign_add) window["Sign_rank"].sign_add();		
+});
+
